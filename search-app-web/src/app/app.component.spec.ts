c77abed5e5284bf226104f6d8e7118f7f358b149
Sanal Nair <sanalkvnair@gmail.com>
2026-06-24 10:29:54 +0200

    fixed search-app-web vulnerabilities

--- search-app-web/src/app/app.component.spec.ts
+++ search-app-web/src/app/app.component.spec.ts
@@ -20,8 +20,8 @@ describe("AppComponent", () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector(".content span")?.textContent).toContain(
-			"myapplication app is running!"
+		expect(compiled.querySelector(".toolbar h1")?.textContent).toContain(
+			"Search App"
 		);
 	});
 });
--- search-app-web/src/app/input/input.component.ts
+++ search-app-web/src/app/input/input.component.ts
@@ -7,6 +7,7 @@ import { QueryData } from "../services/responses/querydata";
 	selector: "app-input",
 	templateUrl: "./input.component.html",
 	styleUrls: ["./input.component.scss"],
+	standalone: false,
 })
 export class InputComponent {
 	loading = false;
